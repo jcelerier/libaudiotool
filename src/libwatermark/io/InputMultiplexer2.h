ea26442cce4ea407ae18527c448ff799ee4c694b
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2014-07-21 18:04:10 +0200

    Disable openmp here for now

--- src/libwatermark/io/InputMultiplexer2.h
+++ src/libwatermark/io/InputMultiplexer2.h
@@ -32,14 +32,15 @@ class InputMultiplexer : public InputManagerBase<data_type>
 
 		virtual Audio_p getNextBuffer() override
 		{
+			// TODO passer en variable de la classe
 			auto out = new MultiData;
 			out->_data.resize(_inputs.size());
 			bool abort{false};
 
-#pragma omp parallel for
+//#pragma omp parallel for
 			for(auto i = 0U; i < _inputs.size(); ++i)
 			{
-#pragma omp flush (abort)
+//#pragma omp flush (abort)
 				if(!abort)
 				{
 					out->_data[i] = _inputs[i]->getNextBuffer();
@@ -47,7 +48,7 @@ class InputMultiplexer : public InputManagerBase<data_type>
 					if(!out->_data[i])
 					{
 						abort = true;
-#pragma omp flush (abort)
+//#pragma omp flush (abort)
 					}
 
 				}
